a60f07ea5c3a53524fbef3c3040d04389b8297d8
muggle <YuanZN@hy.com>
2026-03-26 14:50:20 +0800

    注解注入值得方式

---
+++ ruoyi-common/ruoyi-common-tool/src/main/java/com/ruoyi/common/tool/annotation/MultiUserAssembly.java
@@ -0,0 +1,31 @@
+package com.ruoyi.common.tool.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 多字段用户信息装配注解
+ * <p>
+ * 用于标注需要装配多个用户字段的方法，通过UserAssemblyConfig数组配置多个字段映射。
+ * 适用于需要同时装配多个用户字段的场景，如同时装配创建人、更新人、审批人等。
+ * </p>
+ *
+ * @author 146464
+ * @since 2026-03-04
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MultiUserAssembly {
+
+    /**
+     * 用户装配配置数组
+     * <p>
+     * 配置多个用户字段的装配规则，每个配置项指定一个用户ID字段和对应的用户名字段。
+     * </p>
+     *
+     * @return 用户装配配置数组
+     */
+    UserAssemblyConfig[] value();
+}
---
+++ ruoyi-common/ruoyi-common-tool/src/main/java/com/ruoyi/common/tool/annotation/UserAssemblyConfig.java
@@ -0,0 +1,41 @@
+package com.ruoyi.common.tool.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用户装配配置注解
+ * <p>
+ * 用于配置单个用户字段的装配规则，作为MultiUserAssembly的内部注解使用。
+ * 支持嵌套对象字段路径，如：crmVisitPlanDetailBaseVO.visitorId
+ * </p>
+ *
+ * @author 146464
+ * @since 2026-03-04
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UserAssemblyConfig {
+
+    /**
+     * 用户ID字段名
+     * <p>
+     * 支持嵌套对象字段路径，使用点号分隔，如：crmVisitPlanDetailBaseVO.visitorId
+     * </p>
+     *
+     * @return 用户ID字段名
+     */
+    String userIdField();
+
+    /**
+     * 用户名字段名
+     * <p>
+     * 支持嵌套对象字段路径，使用点号分隔，如：crmVisitPlanDetailBaseVO.visitorName
+     * </p>
+     *
+     * @return 用户名字段名
+     */
+    String userNameField();
+}
