bfda6b74534ae8707fec7e9a5c6f1153b703eba3
muggle <YuanZN@hy.com>
2026-03-26 19:09:00 +0800

    加注解

---
+++ ruoyi-common/ruoyi-common-tool/src/main/java/com/ruoyi/common/tool/query/processor/NotInProcessor.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2019 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ruoyi.common.tool.query.processor;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
+import com.ruoyi.common.tool.annotation.query.ConditionProcessor;
+import com.ruoyi.common.tool.annotation.query.NotIn;
+import com.ruoyi.common.tool.query.query.AbstractQuery;
+import org.springframework.util.StringUtils;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+
+/**
+ *
+ * @param <QUERY>  自定义查询 Query
+ * @param <ENTITY> 查询想对应的实体类型
+ * @author WcJun
+ * @date 2019/05/12
+ */
+@ConditionProcessor(targetAnnotation = NotIn.class)
+public class NotInProcessor<QUERY extends AbstractQuery, ENTITY>
+        extends CriteriaAnnotationProcessorAdaptor<NotIn, QUERY, QueryWrapper<ENTITY>, ENTITY> {
+
+    @Override
+    public boolean process(QueryWrapper<ENTITY> queryWrapper, Field field, QUERY query, NotIn criteriaAnnotation) {
+
+        final Collection<Object> value = (Collection) this.columnValue(field, query);
+        if (this.isNullOrEmpty(value)) {
+            // 属性值为 Null OR Empty 跳过
+            return true;
+        }
+
+        String columnName = criteriaAnnotation.alias();
+        if (StringUtils.isEmpty(columnName)) {
+            columnName = this.columnName(field, criteriaAnnotation.naming());
+        }
+        assert columnName != null;
+        queryWrapper.notIn(ObjectUtils.isNotEmpty(value), columnName, value);
+
+        return true;
+    }
+}
